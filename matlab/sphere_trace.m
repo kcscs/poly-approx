b8f37e74e9a413c10f86e7d0f7ae115da81b1cf5
Csongor Karikó <kcscs10@gmail.com>
2024-12-30 01:21:10 +0100

    matlab chebfun test and sphere tracer

---
+++ matlab/sphere_trace.m
@@ -0,0 +1,109 @@
+RES = [32 32];
+FOVY = deg2rad(90);
+EYE = [-4;2;4];
+TARGET = [0;0;0];
+CLIP = [0.1 10.050000000745058];
+
+
+
+
+f = cot(FOVY/2);
+aspect = RES(1)/RES(2);
+near = CLIP(1);
+far = CLIP(2);
+perspective = [f/aspect 0 0 0; 0 f 0 0; 0 0 (far+near)/(near-far) (2*far*near)/((near-far)); 0 0 -1 0];
+
+fwd = EYE-TARGET;
+fwd = fwd / norm(fwd);
+right = cross(fwd,[0;1;0]);
+right = right/norm(right);
+up = cross(right, fwd);
+up = up/norm(up);
+rot = [right,up,fwd];
+view = zeros(4);
+view(1:3,1:3) = transpose(rot);
+view(1:3,4) = -EYE;
+view(4,4) = 1;
+
+
+yedge = tan(FOVY / 2);
+xedge = RES(1)/RES(2)*yedge;
+
+% ERROR: transpose needed
+invView = transpose([0.707107, -0, 0.707107, -0; 0.235702, 0.942809, -0.235702, 0; -0.666667, 0.333333, 0.666667, -0; -4, 2, 4, 1]);
+
+viewProj = perspective * view;
+invViewProj = inv(viewProj);
+
+img = zeros([RES(1),RES(2),3]);
+
+for Y = 1:RES(2)
+    for X = 1:RES(1)
+        %if Y ~= 15 || X ~= 15
+        %    continue
+        %end
+
+       ndc = zeros(4,1);
+       ndc(1) = (X-1)/(RES(1)-1)*2-1;
+       ndc(2) = 1-2*(Y-1)/(RES(2)-1);
+       ndc(4) = 1;
+
+       wp = invViewProj * ndc;
+       wp = wp/wp(4);
+       wp = wp(1:3);
+
+ %    [X,Y]
+%     ndc
+       ahead = [ndc(1)*xedge; ndc(2)*yedge; -1; 1];
+       wp = invView * ahead;
+       wp = wp(1:3,1);
+       %EYE
+
+       rayDir = wp-EYE;
+       rayDir = rayDir/norm(rayDir);
+
+        % TODO: sphere trace here
+        % r should contain the roots as a column vector
+        %r = roots(approx)
+        
+        pos = EYE;
+        d = surface_func(pos);
+        step_idx = 0;
+        max_step_count = 100;
+        while d > 0.1 && step_idx < max_step_count
+            pos = pos + rayDir * d;
+            d = surface_func(pos);
+            step_idx = step_idx + 1;
+        end
+
+       if step_idx < max_step_count
+        
+        wp = pos;
+        n = zeros(3,1);
+        n(1) = surface_func([wp(1) + 0.01; wp(2); wp(3)]) - surface_func([wp(1) - 0.01; wp(2); wp(3)]);
+        n(2) = surface_func([wp(1); wp(2)+ 0.01; wp(3)]) - surface_func([wp(1); wp(2) - 0.01; wp(3)]);
+        n(3) = surface_func([wp(1); wp(2); wp(3) + 0.01]) - surface_func([wp(1); wp(2); wp(3) - 0.01]);
+        n = n/norm(n);
+
+        to_eye = EYE-wp;
+        to_eye = to_eye/norm(to_eye,2);
+        if dot(n, to_eye) < 0
+            n = -1*n;
+        end
+
+          LIGHT_DIR = [0.08105321228504181; -0.7479685544967651;-0.6379234790802002];
+          LIGHT_DIR = LIGHT_DIR/norm(LIGHT_DIR);
+          diffuse = dot(n, -LIGHT_DIR);
+            
+          %img(Y,X,1) = norm(EYE-wp)/5;
+          img(Y,X,1) = diffuse;
+          img(Y,X,2) = diffuse;
+          img(Y,X,3) = diffuse;
+        
+       else
+          img(Y,X,3) = 1;
+       end
+    end
+end
+
+imwrite(img,"asd.png")
